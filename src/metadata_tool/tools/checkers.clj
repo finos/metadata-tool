68093f173188fa5202939830541053e5fb691aa3
Maurizio Pillitu <maoo@finos.org>
2019-09-03 18:25:36 +0200

    Enable ACTIVE state in the checkers
    Working on implementing lifecycle changes approved by the board.
    
    See https://finosfoundation.atlassian.net/wiki/spaces/FINOS/pages/1158053892/RFC+Revised+Project+Lifecycle

--- src/metadata_tool/tools/checkers.clj
+++ src/metadata_tool/tools/checkers.clj
@@ -117,13 +117,13 @@
 
 (defn- check-project-states
   []
-  (let [projects-with-invalid-states (remove #(boolean (some #{(:state %)} ["INCUBATING" "RELEASED" "ARCHIVED"])) (md/projects-metadata))]
+  (let [projects-with-invalid-states (remove #(boolean (some #{(:state %)} ["ACTIVE" "INCUBATING" "RELEASED" "ARCHIVED"])) (md/projects-metadata))]
     (if (> (count projects-with-invalid-states) 0) (ec/set-error))
     (doall (map #(println "❌ Project" (activity-to-string %) "has an invalid state:" (:state %)) projects-with-invalid-states))))
 
 (defn- check-working-group-states
   []
-  (let [working-groups-with-invalid-states (remove #(boolean (some #{(:state %)} ["OPERATING" "PAUSED" "ARCHIVED"])) (md/working-groups-metadata))]
+  (let [working-groups-with-invalid-states (remove #(boolean (some #{(:state %)} ["ACTIVE" "OPERATING" "PAUSED" "ARCHIVED"])) (md/working-groups-metadata))]
     (if (> (count working-groups-with-invalid-states) 0) (ec/set-error))
     (doall (map #(println "❌ Working Group" (activity-to-string %) "has an invalid state:" (:state %)) working-groups-with-invalid-states))))
 
