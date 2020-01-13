5c49474abb7675c6242fc104a4e90e6a9ac74a5d
Maurizio Pillitu <maoo@finos.org>
2020-01-13 17:30:24 +0100

    revert changes for local reporting

--- src/metadata_tool/tools/reports.clj
+++ src/metadata_tool/tools/reports.clj
@@ -82,17 +82,17 @@
 (defn- send-email-to-pmc
   [program-id subject body]
   ; TODO - enable code below and comment out print commands!
-  (println "===========================")
-  (println subject)
-  (println "---------------------------")
-  (println body)
-  (println "==========================="))
-  ; (if-not (str/blank? program-id)
-  ;   (send-email (:pmc-mailing-list-address (md/program-metadata program-id))
-  ;               subject
-  ;               body
-  ;               :cc-addresses     program-liaison-email-address
-  ;               :reply-to-address program-liaison-email-address)))
+  ; (println "===========================")
+  ; (println subject)
+  ; (println "---------------------------")
+  ; (println body)
+  ; (println "==========================="))
+  (if-not (str/blank? program-id)
+    (send-email (:pmc-mailing-list-address (md/program-metadata program-id))
+                subject
+                body
+                :cc-addresses     program-liaison-email-address
+                :reply-to-address program-liaison-email-address)))
 
 (defn participation-img
   [type program]
@@ -114,7 +114,9 @@
   (let [now-str                                          (tf/unparse (tf/formatter "yyyy-MM-dd h:mmaa ZZZ") (tm/now))
         all-programs                                     (md/programs-metadata)
         six-months-ago                                   (tm/minus (tm/now) (tm/months 6))
-
+        ; to build local report based on last 12 months of activity
+        ; six-months-ago                                   (tm/minus (tm/now) (tm/months 12))
+        
         unarchived-activities-without-leads              (group-by :program-id
                                                                    (remove #(= "ARCHIVED" (:state %))
                                                                            (filter #(str/blank? (:lead-or-chair-person-id %))
